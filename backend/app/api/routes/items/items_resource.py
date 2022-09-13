565ae47f988b4534be5886bf1fbb0646e53a03ef
Isaac Z Tai <39180701+powerpufffs@users.noreply.github.com>
2022-09-12 21:57:34 -0600

    hi

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -78,9 +78,9 @@ async def create_new_item(
         body=item_create.body,
         seller=user,
         tags=item_create.tags,
-        image=item_create.image
-        if item_create.image != ""
-        else "https://via.placeholder.com/150",
+        image="https://via.placeholder.com/150"
+        # if item_create.image != ""
+        # else "https://via.placeholder.com/150",
     )
     send_event('item_created', {'item': item_create.title})
     return ItemInResponse(item=ItemForResponse.from_orm(item))
