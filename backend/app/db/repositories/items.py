bfac5b3d9950e2d8ad9309683fe4c25c7528fc1a
Isaac Z Tai <39180701+powerpufffs@users.noreply.github.com>
2022-09-13 22:21:30 -0600

    Add title param and filtering on ilike on it

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -50,7 +50,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                 description=description,
                 body=body,
                 seller_username=seller.username,
-                image=image
+                image=image,
             )
 
             if tags:
@@ -103,6 +103,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
     async def filter_items(  # noqa: WPS211
         self,
         *,
+        title: Optional[str] = None,
         tag: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
@@ -134,6 +135,8 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
             ).as_(
                 SELLER_USERNAME_ALIAS,
             ),
+        ).where(
+            items.title.ilike(f'%{title}%')
         )
         # fmt: on
 
@@ -205,7 +208,9 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         items_rows = await self.connection.fetch(query.get_sql(), *query_params)
 
         return [
-            await self.get_item_by_slug(slug=item_row['slug'], requested_user=requested_user)
+            await self.get_item_by_slug(
+                slug=item_row["slug"], requested_user=requested_user
+            )
             for item_row in items_rows
         ]
 
@@ -257,9 +262,9 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         return [row["tag"] for row in tag_rows]
 
     async def get_favorites_count_for_item_by_slug(self, *, slug: str) -> int:
-        return (
-            await queries.get_favorites_count_for_item(self.connection, slug=slug)
-        )["favorites_count"]
+        return (await queries.get_favorites_count_for_item(self.connection, slug=slug))[
+            "favorites_count"
+        ]
 
     async def is_item_favorited_by_user(self, *, slug: str, user: User) -> bool:
         return (
@@ -300,8 +305,8 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         title_query = Query.from_(items).select(items.title).where(items.slug == slug)
         result_rows = await self.connection.fetch(title_query.get_sql())
         if not len(result_rows):
-            raise Exception(f'No item with slug {slug}')
-        title = result_rows[0]['title']
+            raise Exception(f"No item with slug {slug}")
+        title = result_rows[0]["title"]
 
         return Item(
             id_=item_row["id"],
